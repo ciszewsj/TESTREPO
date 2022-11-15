3038566902cd4472fbb2756f245d1cb665baa738
jciszewski <jciszewski@amusys.pl>
2022-11-15 09:20:57 +0100

    s

---
+++ src/main/java/com/example/testrepo1/Testrepo1Application.java
@@ -0,0 +1,13 @@
+package com.example.testrepo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testrepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testrepo1Application.class, args);
+	}
+
+}
