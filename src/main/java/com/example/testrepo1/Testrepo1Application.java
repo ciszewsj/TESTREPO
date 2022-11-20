7fdc2f7263f397a4ea14c7f433fbef93c1b2a09b
jciszewski <jciszewski@amusys.pl>
2022-11-20 01:50:15 +0100

    f

--- src/main/java/com/example/testrepo1/Testrepo1Application.java
+++ src/main/java/com/example/testrepo1/Testrepo1Application.java
@@ -2,8 +2,10 @@ package com.example.testrepo1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+
 public class Testrepo1Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/testrepo1/get/GetController.java
+++ src/main/java/com/example/testrepo1/get/GetController.java
@@ -1,5 +1,7 @@
 package com.example.testrepo1.get;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,20 +11,38 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RequestMapping
 public class GetController {
+	@Value("${application.name}")
+	private String applicationName;
+
+	@Value("${build.version}")
+	private String buildVersion;
+
+	@Value("${build.timestamp}")
+	private String buildTimestamp;
+
 	@GetMapping()
-	public String get(){
+	public String get() {
 		return "Hello world!";
 	}
 
+	@GetMapping("/info")
+	public String info() {
+		return "AppName: " + applicationName + "\nVersion: " + buildVersion + "\nTime: " + buildTimestamp;
+	}
+
+
 	@GetMapping("/message")
-	public String getsecond(){
+	public String getsecond() {
 		return "Hello Second World!";
 	}
+
 	@GetMapping("/test")
-	public String test(){
+	public String test() {
 		return "Hello Second Test World!";
-	}@GetMapping("/test2")
-	public String test2(){
+	}
+
+	@GetMapping("/test2")
+	public String test2() {
 		return "Hello Second Test World!";
 	}
 }
